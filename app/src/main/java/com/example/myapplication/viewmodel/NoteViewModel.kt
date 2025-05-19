abacba37755d3ab6a4671a5ccc0d5b3e5eb341e0
Debjit Saha <debjitsaha606@gmail.com>
2025-05-19 19:09:34 +0530

    note view model added

---
+++ app/src/main/java/com/example/myapplication/viewmodel/NoteViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.myapplication.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.room.Query
+import com.example.myapplication.model.Note
+import com.example.myapplication.repository.NoteRepository
+import kotlinx.coroutines.launch
+
+class NoteViewModel(app:Application,private val noteRepository: NoteRepository):AndroidViewModel(app) {
+    fun addNote(note: Note)=
+        viewModelScope.launch {
+            noteRepository.insertNote(note)
+        }
+
+    fun deleteNote(note: Note) =
+        viewModelScope.launch {
+            noteRepository.updateNote(note)
+        }
+
+    fun getAllNote() = noteRepository.getAllNotes()
+
+    fun searchNote(query: String?)=
+        noteRepository.searchNote(query)
+}
